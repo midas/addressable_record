7da22cf850c8c008f31d295c5279041fa4951999
Jason Harrelson <jason@lookforwardenterprises.com>
2010-03-21 11:44:36 -0500

    Release 1.0.3.

--- lib/addressable_record.rb
+++ lib/addressable_record.rb
@@ -4,7 +4,7 @@ require 'addressable_record/address'
 require 'addressable_record/active_record_extesions'
 
 module AddressableRecord
-  VERSION = '1.0.2'
+  VERSION = '1.0.3'
 end
 
 ActiveRecord::Base.send( :include, AddressableRecord::ActiveRecordExtensions ) if defined?( ActiveRecord::Base )
